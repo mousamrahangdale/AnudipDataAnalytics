d1b8cc41ad9bc9f07b31e30efa058ff93e6aff4d
Mousam Rahangdale <mousamrahangdale2003@gmail.com>
2025-02-28 16:29:53 +0530

    Update first.py

--- Code_Python/16JanConditionalStatement/first.py
+++ Code_Python/16JanConditionalStatement/first.py
@@ -1,1 +1,7 @@
+#1 Write a Python program that takes a number as input and prints "Even" if the number is even and "Odd" if it's odd.
 
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
